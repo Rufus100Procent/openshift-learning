beda2bfad064429d8fbe8b14421c612cd6d16df0
stykle <Maxamadabdulahist@outlook.com>
2025-11-29 11:46:12 +0100

    Initial commit

---
+++ src/main/java/com/example/openshiftlearning/OpenshiftLearningApplication.java
@@ -0,0 +1,21 @@
+package com.example.openshiftlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class OpenshiftLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenshiftLearningApplication.class, args);
+    }
+
+    @GetMapping
+    public String api() {
+        return "Hello World";
+    }
+
+}
